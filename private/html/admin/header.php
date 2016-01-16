b87f6423b022eb78e27eec973b595357cf02a8ce
Luke Shiner <luke@lukeshiner.com>
2016-01-16 14:28:17 +0000

    Adds framework for admin area

---
+++ private/html/admin/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>TJ Right Direction</title>
+    <link rel="stylesheet" href="/css/tjrightdirection_admin.css">
+</head>
+<body>
+    <div id="container">
+        <div id="header">
+            <h1>TJ Right Direction Admin</h1>
+<?php include('nav.php'); ?>
+        </div>
+        <div id="body">
---
+++ private/html/admin/nav.php
@@ -0,0 +1,5 @@
+            <ul class="nav">
+                <li class="nav"><a class="nav" href="gallery.php">Gallery</a></li>
+                <li class="nav"><a href="recommendations.php" class="nav">Recommendations</a></li>
+                <li class="nav"><a href="contact.php" class="nav">Contact Us</a></li>
+            </ul>
