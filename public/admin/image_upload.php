dd84c14533e7ad30262616126bf842167744b917
Luke Shiner <luke@lukeshiner.com>
2016-01-24 21:36:20 +0000

    Improvements to image upload form and tweeks to image quality

--- public/admin/image_upload.php
+++ public/admin/image_upload.php
@@ -6,8 +6,6 @@ require_once($PRIVATE . 'html/admin/header.php');
 $database = new tjrightdirection\Database();
 $jobNames = $database->getJobNames();
 
-print_r($jobNames);
-
 if (isset($_FILES['image'])) {
     $jobName = $_POST['job_name'];
     if (array_key_exists($jobName, $jobNames)) {
@@ -23,12 +21,12 @@ if (isset($_FILES['image'])) {
     $filepath = $GALLERY_FULL_PATH . $filename;
     move_uploaded_file($_FILES['image']['tmp_name'], $filepath);
     $fullResize = new tjrightdirection\Gallery\Resize($filepath);
-    $fullResize->resizeImage(1000, 1000, 'auto');
-    $fullResize->saveImage($filepath, 100);
+    $fullResize->resizeImage(800, 800, 'auto');
+    $fullResize->saveImage($filepath, 80);
     $thumbResize = new tjrightdirection\Gallery\Resize($filepath);
     $thumbResize -> resizeImage(200, 200, 'auto');
     $thumbPath = $GALLERY_THUMB_PATH . $filename;
-    $thumbResize->saveImage($thumbPath, 100);
+    $thumbResize->saveImage($thumbPath, 60);
     $database->addImageToGallery($filename, $jobID, $sortNumber);
 }
 ?>
@@ -40,12 +38,12 @@ if (isset($_FILES['image'])) {
         </tr>
         <tr>
             <td><label for="job_name_new">Job Name</label></td>
-            <td><input type="text" name="job_name"></td>
+            <td><input type="text" name="job_name_new" id="job_name_new"></td>
         </tr>
         <tr>
             <td><label for="job_name_old">Existing Job</label></td>
             <td>
-                <select name="job_name_old" id="">
+                <select name="job_name_old" id="job_name_old">
                     <option label=" "></option>
                     <?php
                     foreach($jobNames as $job => $jobID) {
@@ -60,8 +58,34 @@ if (isset($_FILES['image'])) {
             <td><input type="submit" name='submit' title='submit' alt='submit' value='Submit'></td>
         </tr>
     </table>
-
-
+    <input type="text" name="job_name" id="job_name" hidden>
 </form>
+
+<script>
+    $('#job_name_new').change(function() {
+        $('#job_name_old').val(" ");
+    });
+
+    $('#job_name_old').change(function() {
+        $('#job_name_new').val('');
+    });
+
+    $('#image_upload_form').submit(function(e) {
+        if (($('#job_name_new').val() === '') && ($('#job_name_old') === '')) {
+            alert("Please enter either a new job name or select an existing one.");
+            return false;
+        } else if ($('#job_name_new').val() !== '') {
+            $('#job_name').val($('#job_name_new').val());
+            return true;
+        } else if ($('#job_name_old').val() !== '') {
+            $('#job_name').val($('#job_name_old').val());
+            return true;
+        } else {
+            alert("Please enter either a new job name or select an existing one.");
+            return false;
+        }
+
+    });
+</script>
 <?php
 require_once($PRIVATE . 'html/admin/footer.php');
