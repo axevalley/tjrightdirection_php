39d2f748712910f128c1035fbec21a32caab6c38
Luke Shiner <luke@lukeshiner.com>
2016-02-02 12:25:08 +0000

    Further modification to .gitingore

--- public/admin/scratch.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once(dirname($_SERVER['DOCUMENT_ROOT']) . '/private/config.php');
-require_once($PRIVATE . 'html/admin/header.php');
-$database = new tjrightdirection\Database();
-
-$database = new tjrightdirection\Database();
-
-$images = $database->getAllImages();
