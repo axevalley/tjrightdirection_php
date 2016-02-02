b1571f6b1e1276b988a7fd5d00ccb9a8c209bee8
Luke Shiner <luke@lukeshiner.com>
2016-02-02 13:54:03 +0000

    Adds Email

---
+++ private/lib/tjrightdirection/Email.php
@@ -0,0 +1,24 @@
+<?php
+namespace tjrightdirection;
+
+class Email extends \LSPHP\Email
+{
+    public function __construct()
+    {
+        $this->host = "http://mail.tjrightdirection.co.uk";
+        $this->username = "webform@tjrightdirection.co.uk";
+        $this->password = "S6L7!Z4W";
+        $this->port = "587";
+        $this->from = "webform@tjrightdirection.co.uk";
+    }
+
+    public function sendContactFormMessage($name, $message)
+    {
+        $to = "axevalley@hotmail.co.uk";
+        $reply = "webform@tjrightdirection.co.uk";
+        $subject = "Web Form Message From " . $name;
+        $body = $message;
+
+        return $this->sendMail($to, $subject, $body, $reply = null);
+    }
+}
